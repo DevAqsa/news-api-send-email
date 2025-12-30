12aca3f8a3f5b2ee883ddacaf2d84275b28f78a3
Aqsa <codewithaqsa@gmail.com>
2025-12-30 15:14:04 +0500

    send the email to mygmail account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "d477a92e969944babdaa79e70a11c579"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2025-11-30&sortBy=publishedAt&apiKey=d477a92e969944babdaa79e70a11c579"
@@ -11,7 +12,9 @@ request = requests.get(url)
 content = request.json()
 
 # access a article titles and descriptions
+body = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    body += article['title'] + "\n" + article['description'] + "\n"
 
+body = body.encode("utf-8")
+send_email(message=body)
